77f7e3b5038b514dc80fcee86beffbfa526f1f22
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 15:34:15 +0530

    Left Section: Logo The leftmost element is a heading that serves as the app's logo. It displays the text "YTShorts", with "Shorts" styled in red to visually distinguish it.

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -17,7 +17,12 @@ type Props = {};
 const Navbar = (props: Props) => {
   return (
     <div className="flex items-center justify-between h-14">
-      
+      {/* Logo text */}
+      <div>
+        <h1 className="font-bold text-xl">
+          YT<span className="text-red-500">Shorts</span>
+        </h1>
+      </div>
       
       
     </div>
