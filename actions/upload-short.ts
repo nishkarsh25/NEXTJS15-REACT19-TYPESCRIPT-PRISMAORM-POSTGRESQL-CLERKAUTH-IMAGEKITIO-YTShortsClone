bcc46b7c723eda57a2e0ad1f0f8cda06c43c5490
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 15:25:54 +0530

    Zod Schema Definition A schema is created using Zod to validate incoming form data. It expects three fields:
    A title with a minimum of 3 characters.
    
    A description with a minimum of 5 characters.
    
    A video string, likely a video URL or identifier (e.g., from an uploaded file or link).

--- actions/upload-short.ts
+++ actions/upload-short.ts
@@ -5,6 +5,10 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
-
+const uploadShortsSchema = z.object({
+  title: z.string().min(3),
+  description: z.string().min(5),
+  video: z.string(),
+});
 
 
