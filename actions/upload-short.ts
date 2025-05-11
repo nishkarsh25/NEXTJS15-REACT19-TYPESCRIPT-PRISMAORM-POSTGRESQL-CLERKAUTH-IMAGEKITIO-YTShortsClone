29c81fd80c92c3d0ea5004000df51514ca89e6e8
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 15:25:47 +0530

    Imports and Dependencies The function relies on several imported tools:
    A Prisma client to interact with the database.
    
    Clerk's server-side auth to get the currently logged-in user's ID.
    
    Utilities from Next.js for cache invalidation and navigation redirection.
    
    Zod, a schema validation library, is used to define and validate the structure of incoming form data.

--- actions/upload-short.ts
+++ actions/upload-short.ts
@@ -1,5 +1,9 @@
 "use server";
-
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
 
 
 
