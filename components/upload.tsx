2d0f17313b551bf7f93b76616e53e1f7a5bd0f4a
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 16:40:27 +0530

    Authenticator Function This async function calls a custom API route /api/auth to get:
    signature
    
    expire
    
    token
    
    These are required for secure authenticated uploads to ImageKit.
    
    Handles and throws detailed errors if the request fails.

--- components/upload.tsx
+++ components/upload.tsx
@@ -7,7 +7,27 @@ import { Progress } from "./ui/progress";
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
+const authenticator = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/auth");
 
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
+    }
+
+    const data = await response.json();
+    const { signature, expire, token } = data;
+    return { signature, expire, token };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(`Authentication request failed: ${error.message}`);
+    }
+    throw error;
+  }
+};
 
 
 
