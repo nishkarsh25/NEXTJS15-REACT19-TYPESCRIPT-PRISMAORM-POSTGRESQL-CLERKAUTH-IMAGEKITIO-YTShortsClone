9ce3801c92acfad21569d0268bd55227c65c1b2a
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 16:16:38 +0530

    Environment Variable for ImageKit The urlEndPoint is retrieved from environment variables. It defines the base URL from which ImageKit serves videos or images. This allows the code to dynamically calculate the correct path by stripping the endpoint prefix from the full URL.

--- components/shorts/short-card.tsx
+++ components/shorts/short-card.tsx
@@ -7,6 +7,7 @@ import { IKVideo, ImageKitProvider } from "imagekitio-next";
 import { Avatar } from "../ui/avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT as string;
 
 
 
