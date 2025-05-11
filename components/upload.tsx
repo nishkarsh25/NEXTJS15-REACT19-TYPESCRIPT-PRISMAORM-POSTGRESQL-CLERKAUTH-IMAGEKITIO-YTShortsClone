26e1e94cb39297c13e85d8040127a03f677dab1f
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 16:40:11 +0530

    Environment Variables Pulls:
    publicKey: Used for authentication with ImageKit.
    
    urlEndpoint: The base URL for the ImageKit CDN where files are stored.

--- components/upload.tsx
+++ components/upload.tsx
@@ -1,7 +1,11 @@
 "use client";
+import React, { useRef, useState } from "react";
+import { ImageKitProvider, IKImage, IKUpload } from "imagekitio-next";
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
+import { Progress } from "./ui/progress";
 
-
-
+const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
 
 
 
