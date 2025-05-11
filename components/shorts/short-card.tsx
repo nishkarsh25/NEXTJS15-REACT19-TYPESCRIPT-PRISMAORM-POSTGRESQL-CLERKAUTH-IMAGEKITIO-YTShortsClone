564ca4abce994e69213bf6b99c813a5a53e9e58f
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 16:16:30 +0530

    Prisma Type Import Prisma’s ShortsGetPayload utility is used to strongly type the short prop — including not just the short’s fields but also related user information.
    React
    The base library for writing the component.
    
    Card UI Components
    Card and CardFooter are used to build a structured and styled card layout. These are likely from your custom UI library.
    
    These components help render content inside a box with padding, shadows, and a defined layout.
    
    ImageKit (Video Hosting)
    ImageKitProvider sets up configuration for using ImageKit in the app.
    
    IKVideo renders the actual video from ImageKit, applying transformations (like resolution or format).
    
    This allows for performance-optimized, CDN-hosted video delivery.
    
    Avatar Components
    Avatar, AvatarImage, and AvatarFallback are used to show the user’s profile photo.
    
    If an image is missing or fails to load, a fallback (like initials) is shown instead.

--- components/shorts/short-card.tsx
+++ components/shorts/short-card.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-
+import { Prisma } from "@prisma/client";
+import React from "react";
+import { Card, CardFooter } from "../ui/card";
+import { IKVideo, ImageKitProvider } from "imagekitio-next";
+import { Avatar } from "../ui/avatar";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 
 
