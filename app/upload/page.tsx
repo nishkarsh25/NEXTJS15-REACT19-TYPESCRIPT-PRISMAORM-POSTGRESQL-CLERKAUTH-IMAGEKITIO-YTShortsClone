c6891e3317911e07de1c0befb54e3ad895a007a2
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 16:40:01 +0530

    Imports React-related: Hooks like useState and useRef to manage UI state and references.
    ImageKit SDK components:
    
    ImageKitProvider: Configures access to ImageKit.
    
    IKUpload: A prebuilt file upload component.
    
    IKImage: Not used in the current code but typically for rendering images.
    
    Types: IKUploadResponse gives a type-safe response object from ImageKit after upload.
    
    Progress: A custom UI progress bar component from the ui/progress module.

---
+++ app/upload/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { uploadShortAction } from "@/actions/upload-short";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Upload from "@/components/upload";
+import { Loader2 } from "lucide-react";
+import React, { useActionState, useState } from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  const [formState, action, isPending] = useActionState(uploadShortAction, {
+    errors: {},
+  });
+
+  const [videoUrl, setVideoUrl] = useState<string>("");
+
+  const handleSubmit = (formData: FormData) => {
+    formData.append("video", videoUrl);
+    return action(formData);
+  };
+
+  return (
+    <div className="max-w-md mx-auto p-6">
+      <h1 className="mb-6 text-2xl font-bold text-center">Upload Shorts</h1>
+      <form action={handleSubmit}>
+        <div className="mb-4">
+          <Label>Title</Label>
+          <Input
+            type="text"
+            name="title"
+            placeholder="Title"
+            className="mt-1"
+          />
+          {formState.errors.title && (
+            <span className="text-red-500 text-sm">
+              {formState.errors.title}
+            </span>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <Label>Description</Label>
+          <Input
+            type="text"
+            name="description"
+            placeholder="Description"
+            className="mt-1"
+          />
+          {formState.errors.description && (
+            <span className="text-red-500 text-sm">
+              {formState.errors.description}
+            </span>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <Upload setVideoUrl={setVideoUrl} />
+          {
+            formState.errors.formError && (
+              <div className="border border-red-500 bg-red-100">
+                <p className="text-red-600">{formState.errors.formError}</p>
+              </div>
+            )
+          }
+        </div>
+
+        <Button type="submit" className="w-full">
+          {isPending ? <Loader2 className="animate-spin h-4 w-4" /> : "Upload"}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default page;
