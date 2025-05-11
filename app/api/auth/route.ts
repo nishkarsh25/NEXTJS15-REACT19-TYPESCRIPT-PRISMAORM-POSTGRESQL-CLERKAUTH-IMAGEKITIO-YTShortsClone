c8c467825e246987f2d910a77d2cf99fd9810a3f
nishkarsh25 <bm21btech11016@iith.ac.in>
2025-05-11 17:26:36 +0530

    ImageKit Configuration An imagekit instance is created using the ImageKit class. It is initialized with three critical configuration values:
    Public Key: This is a client-facing key that is safe to expose. It’s used for operations like uploads or rendering URLs.
    
    Private Key: This must be kept on the server and is used to generate secure authentication signatures.
    
    URL Endpoint: The base URL provided by ImageKit for accessing your files. It’s used when constructing final media URLs.
    
    These configuration values are read from environment variables, ensuring sensitive keys are not hardcoded into the source code.
    
    The exclamation marks (!) after the variable names are TypeScript non-null assertions, telling the compiler that these values are definitely present at runtime, even if TypeScript's type system can't guarantee that statically.

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,5 +1,9 @@
 import ImageKit from "imagekit"
 import { NextResponse } from "next/server";
 
-
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
 
